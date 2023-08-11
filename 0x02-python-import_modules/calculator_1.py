ba2da1d7f3231b370d173c864ab1e70f2fc38613
Yandelf00 <zakaria.afyf00@gmail.com>
2023-08-12 00:20:26 +0100

    calc

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+def add(a, b):
+    """an add function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        Returns the value of a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """a sub function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        Returns the value of a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """a mul function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        Returns the value of a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """a div function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        Returns the value of a / b
+    """
+    return int(a / b)
