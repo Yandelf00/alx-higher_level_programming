23cbf3e7ba300dee475e743395f7e7ddda8d37a3
Yandelf00 <zakaria.afyf00@gmail.com>
2023-08-11 23:59:07 +0100

    add

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """an add function
+
+    Args:
+        a: first int
+        b: second int
+
+    Returns:
+        Returns the value of a + b
+    """
+    return (a + b)
