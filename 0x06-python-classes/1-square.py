e14d44736c823a091a3a4cbba947f27dbcbb3fb3
Yandelf00 <zakaria.afyf00@gmail.com>
2023-08-29 23:15:34 +0100

    add

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""our square class"""
+
+
+class Square:
+    """inside square class"""
+    def __init__(self, size):
+        """init. square
+        Args:
+            size (int): square's size.
+        """
+        self.__size = size
