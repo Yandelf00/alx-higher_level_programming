31ddcce5da0a276f35468e29488f54b666a9047b
Yandelf00 <zakaria.afyf00@gmail.com>
2023-08-29 23:49:52 +0100

    add

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """init square
+        Args:
+            size (int): square size.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """calc area
+        returns:
+            return area.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """get value attribute.
+
+        Returns:
+            int: the attribute value.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets value into size, must be int.
+
+        Args:
+            value (int): square size.
+
+        Raises:
+            TypeError: if not int.
+            ValueError: if not positive.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
