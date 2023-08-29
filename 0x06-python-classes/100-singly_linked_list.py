c3273de351696a07d25797e18691172b629d75f8
Yandelf00 <zakaria.afyf00@gmail.com>
2023-08-30 00:13:30 +0100

    add

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,33 @@
+#!/usr/python3
+"""classe node"""
+
+
+class Node:
+    """classe node"""
+    def data(self):
+        """data
+        returns:
+            ntg for now
+        """
+        pass
+
+    def next_node(self):
+        """next_node
+        returns:
+            ntg for now
+        """
+        pass
+
+    def next_node(self, value):
+        """next_node
+        returns:
+            ntg for now
+        """
+        pass
+
+    def __init__(self):
+        """init node
+        Args:
+            ntg
+        """
+        pass
