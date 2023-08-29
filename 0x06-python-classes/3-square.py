c504edabf04e2a12ba91e7dc9221423c57c180d6
Yandelf00 <zakaria.afyf00@gmail.com>
2023-08-29 23:37:34 +0100

    add

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""our square class"""
+
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """init square
+        Args:
+            size (int): square size.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calc area
+        returns:
+            area.
+        """
+        return self.__size ** 2
