8c195192ffdd7ef28caec58075c905e65e024c2f
Yandelf00 <zakaria.afyf00@gmail.com>
2023-09-11 22:56:04 +0100

    add

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def self(area):
+        """function that raises error cuz !area"""
+        raise Exception("area() is not implemented")
