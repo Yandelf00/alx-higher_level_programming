8c54483453dc80c1b13d3cceae46b159248ea95c
Yandelf00 <zakaria.afyf00@gmail.com>
2023-09-11 23:16:04 +0100

    add

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """function that raises error cuz !area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function that validates idk"""
+        if type(value) not int:
+            raise Exception(f"{name} must be an integer")
+        elif value <= 0:
+            raise Exception(f"{name} must be greater than 0")
