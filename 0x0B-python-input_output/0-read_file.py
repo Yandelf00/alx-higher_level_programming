cfecbc598a6e534f4217e3b82d5bcbcde2529748
Yandelf00 <zakaria.afyf00@gmail.com>
2023-09-12 19:04:37 +0100

    add

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """readfile"""
     if filename == "":
         return
-    with open(filename, encoding="utf-8") as f:
+    with open(filename,'r', encoding="utf-8") as f:
         print(f.read(), end='')
