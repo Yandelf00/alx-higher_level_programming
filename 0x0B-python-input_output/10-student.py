16971478661702aabaf4ac7d1fdcc5a18becb084
Yandelf00 <zakaria.afyf00@gmail.com>
2023-09-12 19:36:55 +0100

    add

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Class Student """
+"""file"""
 
 
 class Student:
+    """Student."""
+
     def __init__(self, first_name, last_name, age):
-        """ init"""
+        """init"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """to_json"""
-        return vars(self)
+        """to_json."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
