b931f75700eac503da057c1c3cf14c636a165590
Yandelf00 <zakaria.afyf00@gmail.com>
2023-09-12 19:32:53 +0100

    add

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""classtojson function"""
+"""Class Student """
 
 
-def class_to_json(obj):
-    """class_to_json"""
-    return vars(obj)
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json"""
+        return vars(self)
