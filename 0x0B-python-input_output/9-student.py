07e5175730d45f64908d07cad3308b63a5e8aae5
Yandelf00 <zakaria.afyf00@gmail.com>
2023-09-12 19:31:22 +0100

    add

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import json
-from sys import argv
-import os
+"""Class Student """
 
 
-if __name__ == '__main__':
-    save_json_file = __import__("7-save_to_json_file").save_to_json_file
-    load_json_file = __import__("8-load_from_json_file").load_from_json_file
-    if not os.path.exists("add_item.json"):
-        with open("add_item.json", "a") as f:
-            pass
-        save_json_file([], "add_item.json")
-    save_json_file(load_json_file("add_item.json") + argv[1:], "add_item.json")
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ to_json"""
+        return vars(self)
