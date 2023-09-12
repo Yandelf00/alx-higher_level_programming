cbc9f4d982adf46b4d83f44e4b7bc149a9ba46bd
Yandelf00 <zakaria.afyf00@gmail.com>
2023-09-12 19:38:42 +0100

    add

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""Class Student """
+"""student classu"""
 
 
 class Student:
+    """Student."""
+
     def __init__(self, first_name, last_name, age):
-        """ init"""
+        """init"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ to_json"""
-        return vars(self)
+    def to_json(self, attrs=None):
+        """to_json."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
+
+    def reload_from_json(self, json):
+        """reload_from_json"""
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
