40de26c557d3a33798042ebac0ad1f46725e8c9c
Yandelf00 <zakaria.afyf00@gmail.com>
2023-09-18 22:22:16 +0100

    add

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,10 +16,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ converts dict to json """
+        """to_json_string.
+
+        :param list_dictionaries:
+        """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        return json.dumps(list_dictionaries)
+        json_string = json.dumps(list_dictionaries)
+        return json_string
 
     @classmethod
     def save_to_file(cls, list_objs):
