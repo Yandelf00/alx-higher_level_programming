bee102e8d0f43f6a2985bbcb929882a727d6ff92
Yandelf00 <zakaria.afyf00@gmail.com>
2023-10-17 21:48:12 +0100

    add

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- shows all the records from the table by score
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- update bobs score to 10 using the name 
+UPDATE second_table 
+SET score = 10
+WHERE name = "Bob";
