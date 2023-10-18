9a307e6ea13807f91d6178e218a458c8a4f3e7ad
Yandelf00 <zakaria.afyf00@gmail.com>
2023-10-18 23:44:34 +0100

    add

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities in the database hbtn_0d_usa.
-SELECT c.`id`, c.`name`, s.`name`
-FROM `cities` AS c
-INNER JOIN `states` AS s ON c.`state_id` = s.`id`
-ORDER BY c.`id`;
+SELECT al.`id`, al.`name`, ooo.`name`
+FROM `cities` AS al
+INNER JOIN `states` AS ooo ON al.`state_id` = ooo.`id`
+ORDER BY al.`id`;
