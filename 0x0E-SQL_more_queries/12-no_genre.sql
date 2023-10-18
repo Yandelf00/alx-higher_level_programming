3638a9a0c323f6a6943e6e70153a3c8f66f639bf
Yandelf00 <zakaria.afyf00@gmail.com>
2023-10-19 00:09:37 +0100

    add

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all shows in the database hbtn_0d_tvshows without a genre linked.
 SELECT cho.`title`, go.`genre_id`
 FROM `tv_shows` AS cho
-LEFT JOIN `tv_show_genres` AS go ON s.`id` = go.`show_id`
+LEFT JOIN `tv_show_genres` AS go ON cho.`id` = go.`show_id`
 WHERE go.`genre_id` IS NULL
 ORDER BY cho.`title`, go.`genre_id`;
