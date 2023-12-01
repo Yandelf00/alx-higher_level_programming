646e35432b08fb6b819997d1bcea88a285cde842
Yandelf00 <zakaria.afyf00@gmail.com>
2023-12-01 22:39:24 +0100

    add

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Use requests to get response"""
-import requests as req
+""" Use requests to get response and print """
+import requests
 
 
 if __name__ == "__main__":
-    res = req.get("https://intranet.hbtn.io/status")
+    res = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type:", type(res.text))
     print("\t- content:", res.text)
