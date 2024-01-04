4af71afa9260dbc3823fb4c9e1d3c711f8f9182e
Yandelf00 <zakaria.afyf00@gmail.com>
2024-01-04 01:06:03 +0100

    add

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const filePath = process.argv[2];
 
 if (!filePath) {
   console.error('Usage: node 0-readme.js <file_path>');
-  process.exit(1); // Exit with an error code
+  process.exit(1);
 }
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
